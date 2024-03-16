d740dbac581e86a6a03a654e2ea30c8911984a7c
EminIT <104794270+EminIT@users.noreply.github.com>
2024-03-17 01:56:02 +0300

    V1.1.0
    root update

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sqlite3
 from colorama import Fore, init
 from bitarray import bitarray
+import os
 
 init() # connect colorama
 
@@ -24,6 +25,10 @@ DELETE <service name>
 PUT <service name>
 SHOW <service name>
 SHOW ALL
+ROOT_ON
+ROOT_OFF
+RENAME_ROOT_PASSWORD
+CLEAR
 EXIT
 	''')
 
@@ -47,6 +52,16 @@ def decode_binary_list(binary_list):
     	decoded.append(item)
     return decoded
 
+
+# check for root
+root = 0 # is root mode enabled?
+root_file = open("data.txt", "r+")
+root_password = decoder(root_file.read())
+
+
+def rename_root_password(new_root_password):
+	root_file.truncate(0)
+	root_file.write(encoder(new_root_password))
 
 
 # commands
@@ -85,32 +100,97 @@ def show(service: str):
 # programm cycle
 while True:
 	try:
-		command = input(">")
+		if root == 0:
+			command = input(">")
+		elif root == 1:
+			command = input("root>")
 
 		if command == "CREATE":
-			auth_url = input("url of registartion(optional): ")
-			programm = input("programm: ")
-			nickname = input("nickname: ")
-			password = input("password: ")
-			create(auth_url, programm, nickname, password)
+			if root == 0:
+				ask_root_password = input(">root: ")
+				if ask_root_password == root_password:
+					auth_url = input("url of registartion(optional): ")
+					programm = input("programm: ")
+					nickname = input("nickname: ")
+					password = input("password: ")
+					create(auth_url, programm, nickname, password)
+				if ask_root_password != root_password:
+					print(">Wrong password.")
+			elif root == 1:
+				auth_url = input("url of registartion(optional): ")
+				programm = input("programm: ")
+				nickname = input("nickname: ")
+				password = input("password: ")
+				create(auth_url, programm, nickname, password)
 
 		if command.startswith(f"DELETE ") and command.replace(f"DELETE ", "") != "":
-			delete(command.replace(f"DELETE ", ""))
+			if root == 0:
+				ask_root_password = input(">root: ")
+				if ask_root_password == root_password:
+					delete(command.replace(f"DELETE ", ""))
+				if ask_root_password != root_password:
+					print(">Wrong password.")
+			elif root == 1:
+				delete(command.replace(f"DELETE ", ""))
 
 
 		if command.startswith(f"PUT ") and command.replace(f"PUT ", "") != "":
-			auth_url = input("url of registartion(optional): ")
-			nickname = input("nickname: ")
-			password = input("password: ")
-			put(auth_url, command.replace(f"PUT ", ""), nickname, password)
+			if root == 0:
+				ask_root_password = input(">root: ")
+				if ask_root_password == root_password:
+					auth_url = input("url of registartion(optional): ")
+					nickname = input("nickname: ")
+					password = input("password: ")
+					put(auth_url, command.replace(f"PUT ", ""), nickname, password)
+				if ask_root_password != root_password:
+					print(">Wrong password.")
+			elif root == 1:
+				auth_url = input("url of registartion(optional): ")
+				nickname = input("nickname: ")
+				password = input("password: ")
+				put(auth_url, command.replace(f"PUT ", ""), nickname, password)
 
 		if command.startswith(f"SHOW ") and command.replace(f"SHOW ", "") != "":
-			print(show(command.replace(f"SHOW ", "")))
+			if root == 0:
+				ask_root_password = input(">root: ")
+				if ask_root_password == root_password:
+					print(show(command.replace(f"SHOW ", "")))
+				if ask_root_password != root_password:
+					print(">Wrong password.")
+			elif root == 1:
+				print(show(command.replace(f"SHOW ", "")))
+
+
+		if command == "ROOT_ON":
+			ask_root_password = input(">root: ")
+			if ask_root_password == root_password:
+				root = 1
+
+
+		if command == "ROOT_OFF":
+			root = 0
+
+
+		if command == "RENAME_ROOT_PASSWORD":
+			ask_root_password = input(">root: ")
+			if ask_root_password == root_password:
+				ask_new_root_password = input(">new root password: ")
+				rename_root_password(ask_new_root_password)
+				print("Restart programm.")
+			if ask_root_password != root_password:
+				print(">Wrong password.")
+
+
+		if command == "CLEAR":
+			os.system('cls||clear')
 
 
 		if command == "EXIT":
 			exit()
+
+
 	except TypeError:
 		print("404")
 
+root_file.close()
 db_cursor.close()
